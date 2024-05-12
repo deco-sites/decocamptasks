3acdee7886cacebe7cf4a2495116a7f0e82c0eed
Matheus Gaudencio do Rêgo <matheusgr@users.noreply.github.com>
2024-05-12 19:59:59 -0300

    content update checks

--- sections/Content.tsx
+++ sections/Content.tsx
@@ -5,7 +5,7 @@ interface Content {
   /**
    * @format rich-text
    */
-  content?: string;
+  content: string;
 }
 
 interface Props {
@@ -21,14 +21,14 @@ export default function Content({ name, content }: Props) {
     <div>
     <aside class="fixed h-full w-60 bg-gray-50 py-8 px-4">
       <div class="space-y-2">
-        {content.map((c, index) => (
+        {content?.map((c, index) => (
           <a href={`#id-${index}`} class="block py-2">{`${c.title}`}</a>
         ))
         }
       </div>
     </aside>
     <div class="grid grid-cols-1 ml-60">
-        {content.map((c, index) => (
+        {content?.map((c, index) => (
           <div id={`id-${index}`} class={index % 2 === 0 ? "bg-gray-100" : "bg-gray-200"} dangerouslySetInnerHTML={{ __html: c.content }}
 ></div>
         ))
