f86054f65a5463dce2ff79b9fea55e149f997557
Matheus Gaudencio do Rêgo <matheusgr@users.noreply.github.com>
2024-05-12 22:48:53 -0300

    fix layout

--- sections/Content.tsx
+++ sections/Content.tsx
@@ -11,18 +11,18 @@ export interface Props {
 export default function Content({ content }: Props) {
   return (
     <div>
-    <aside class="fixed h-full lg:w-60 w-8 bg-gray-50 py-8 px-4">
-      <div class="space-y-2">
+    <aside class="fixed h-full lg:w-60 w-12 bg-gray-50 py-8 lg:px-4 px-2">
+      <div class="space-y-2 overflow-hidden">
         {content?.map((content, index) => (
-          <a href={`#id-${index}`} class="block py-2">{`${content?.title}`}</a>
+          <a href={`#id-${index}`} class="block py-2 truncate">{`${content?.title}`}</a>
         ))
         }
       </div>
     </aside>
-    <div class="flex justify-center lg:ml-60 ml-8 w-full">
+    <div class="flex justify-center lg:ml-60 ml-12 w-full">
         {content?.map((content, index) => (
           <div id={`id-${index}`} class={"py-10 flex justify-center w-full " + (index % 2 === 0 ? "bg-gray-100" : "bg-gray-200")}>
-            <div class="max-w-screen-lg" dangerouslySetInnerHTML={{ __html: content.content }}></div>
+            <div class="p-4 max-w-screen-lg" dangerouslySetInnerHTML={{ __html: content.content }}></div>
           </div>
         ))
         }
