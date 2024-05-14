fe6fad4e2e12bb6d51c583068d5291e4020ba627
Matheus Gaudencio do Rêgo <matheusgr@users.noreply.github.com>
2024-05-13 22:57:04 -0300

    02

--- sections/Content.tsx
+++ sections/Content.tsx
@@ -13,14 +13,14 @@ export default function Content({ content }: Props) {
     <div>
     <aside class="fixed h-full lg:w-60 w-12 bg-gray-50 py-8 lg:px-4 px-2">
       <div class="space-y-2 overflow-hidden">
-        {content?.map((content, index) => (
+        {content?.filter(c => c.show).map((content, index) => (
           <a href={`#id-${index}`} class="block py-2 truncate">{`${content?.title}`}</a>
         ))
         }
       </div>
     </aside>
     <div class="flex justify-center lg:pl-60 pl-12 w-full">
-        {content?.map((content, index) => (
+        {content?.filter(c => c.show).map((content, index) => (
           <div id={`id-${index}`} class={"gap-12 py-10 flex justify-center w-full " + (index % 2 === 0 ? "bg-gray-100" : "bg-gray-200")}>
             <div class="p-4 max-w-screen-lg" dangerouslySetInnerHTML={{ __html: content.content }}></div>
           </div>
